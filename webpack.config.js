370c93c78a92514a57ffa1e46a4e5140e5a4b7af
Andrew Yoo <andrewyoo.ny@gmail.com>
2020-04-06 17:56:35 -0400

    Prepare for initial Heroku push

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     context: __dirname,
-    entry: './frontend/react1.jsx',
+    entry: './frontend/snapper.jsx',
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
