4725de5e7ee757daaecc7327d9b17be6819cf260
Andrew <andrewyoo.ny@gmail.com>
2020-08-26 22:37:39 -0400

    Comment form component added

---
+++ frontend/components/comments/comment_form.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props); 
+        this.state = {
+            body =  ""
+        };
+    }
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        })
+    };
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.props.createComment(this.state)
+            .then( () => this.setState( {body: ""} ))
+    };
+
+    render() {
+        <div className="comment-form">
+            <textarea name="comment-textarea" 
+                      value={this.state.body} 
+                      onChange={this.update(body)}
+                      placeHolder="Add a comment here..." >
+            </textarea>
+
+            <button className="comment-form-submit"
+                    onClick={this.handleSubmit.bind(this)}>
+                    Comment
+            </button>
+        </div>
+    };
+
+}
