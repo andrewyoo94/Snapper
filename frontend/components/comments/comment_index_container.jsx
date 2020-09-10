81374436b5122903a89caa9ac3dbf0897727b439
Andrew <andrewyoo.ny@gmail.com>
2020-09-10 18:24:25 -0400

    When submitting comment it rerenders and shows comment now

--- frontend/components/comments/comment_index_container.jsx
+++ frontend/components/comments/comment_index_container.jsx
@@ -3,11 +3,11 @@ import CommentIndex from './comment_index';
 import { fetchAllComments } from '../../actions/comment_actions';
 import { withRouter } from 'react-router-dom';
 
-
+// added Object.values(photo.comments)
 const mSTP = (state, ownProps) => {
     let photoId = ownProps.match.params.id;
     let photo = state.entities.photos[photoId];
-    let photoComments = photo ? photo.comments : [];
+    let photoComments = photo ? Object.values(photo.comments) : [];
     let users = Object.values(state.entities.users);
      
     return ({
