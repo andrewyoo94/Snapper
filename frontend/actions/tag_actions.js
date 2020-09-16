019a799a220ee542bc796b4894ffb29e00878813
Andrew <andrewyoo.ny@gmail.com>
2020-09-15 21:39:31 -0400

    Added Tag Actions

---
+++ frontend/actions/tag_actions.js
@@ -0,0 +1,45 @@
+import * as TagAPIUtil from '../util/tags_api_util';
+
+export const RECEIVE_TAG = "RECEIVE_TAG";
+export const RECEIVE_ALL_TAGS = "RECEIVE_ALL_TAGS";
+export const RECEIVE_TAG_ERRORS = "RECEIVE_TAG_ERRORS";
+
+const receiveTag = tag => ({
+    type: RECEIVE_TAG,
+    tag
+});
+
+const receiveAllTags = tags => ({
+    type: RECEIVE_ALL_TAGS,
+    tags
+});
+
+const receiveTagErrors = errors => ({
+    type: RECEIVE_TAG_ERRORS,
+    errors
+});
+
+export const fetchTag = (id) => dispatch => {
+    return (
+        TagAPIUtil.fetchTag(id)
+            .then((tag) => dispatch(receiveTag(tag)))
+            .fail(error => dispatch(receiveErrors(error.responseJSON)))
+    )
+}
+
+export const fetchTags = () => dispatch => {
+    return (
+        TagAPIUtil.fetchTags()
+            .then((tags) => dispatch(receiveAllTags(tags)))
+            .fail(error => dispatch(receiveErrors(error.responseJSON)))
+    )
+}
+
+// might have to change tag to an array of tags
+export const createTag = (tag) => dispatch => {
+    return (
+        TagAPIUtil.createTag(tag)
+            .then((tag) => dispatch(receiveTag(tag)))
+            .fail(error => dispatch(receiveErrors(error.responseJSON)))
+    )
+}
