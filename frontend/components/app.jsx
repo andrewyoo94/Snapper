9fa6b104be7037f48fb1c024a2e116771a8c7b12
Andrew <andrewyoo.ny@gmail.com>
2020-09-16 21:28:38 -0400

    Added ProtectedRoute

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Link, Switch } from "react-router-dom";
-import { AuthRoute } from "../util/route_util";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
 
 import GreetingContainer from "./greeting/greeting_container";
 import LoginFormContainer from "./session_form/login_form_container";
@@ -25,7 +25,7 @@ const App = () => (
         <Switch>
             <Route exact path="/explore" component={PhotoIndexContainer} />
             <Route exact path="/photos/:id" component={PhotoShowContainer} />
-            <Route exact path="/upload" component={PhotoUploadContainer} />            
+            <ProtectedRoute exact path="/upload" component={PhotoUploadContainer} />            
         </Switch>
 
         <footer className="footer-master">
@@ -36,8 +36,8 @@ const App = () => (
             <a href="https://www.linkedin.com/in/andrewyoo94/">
                 LinkedIn
             </a>
-
         </footer>
+
     </div>
 );
 
--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -12,6 +12,15 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
     />
 );
 
+const Protected = ({ component: Component, path, loggedIn }) => (
+    <Route
+        path={path}
+        render={props => (
+            loggedIn ? <Component {...props} /> : <Redirect to="/login" />
+        )}
+    />
+);
+
 const mapStateToProps = state => {
     return { loggedIn: Boolean(state.session.id) };
 };
@@ -21,4 +30,11 @@ export const AuthRoute = withRouter(
         mapStateToProps,
         null
     )(Auth)
+);
+
+export const ProtectedRoute = withRouter(
+    connect(
+        mapStateToProps,
+        undefined
+    )(Protected)
 );
