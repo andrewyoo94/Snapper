84bc402981180c45e24898bbb220c55651f59086
Andrew <andrewyoo.ny@gmail.com>
2020-09-22 13:57:38 -0400

    Added tag create component

---
+++ frontend/components/tags/tag_create.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class TagCreate extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            photo_id: this.props.photo_id
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    handleUpdate(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        })
+    };
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('tag[name]', this.state.name);
+        formData.append('tag[photo_id]', this.state.photo_id);
+        this.props.createTag(formData)
+            .then( () => this.setState( {tagName: ''} ))
+    }
+
+    render() {
+        return (
+            <div className="tag-form">
+                <input className="tag-text"
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.handleUpdate("name")}
+                    placeholder="Add a tag..." />
+
+                <button className="tag-button" onClick={this.handleSubmit}>Add tag...</button>
+            </div>
+        )
+    }
+
+}
+
+export default TagCreate;
