653ef14bd13882f2f48f868a06c4cba592cce8f4
Andrew <andrewyoo.ny@gmail.com>
2020-09-27 13:43:54 -0400

    Added tag create to photo show page

--- frontend/components/photos/photo_show.jsx
+++ frontend/components/photos/photo_show.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CommentCreateContainer from '../comments/comment_create_container';
 import CommentIndexContainer from '../comments/comment_index_container';
 import TagShowContainer from '../tags/tag_show_container';
-import TagCreateContainer from '..tags/tag_create_container';
+import TagCreateContainer from '../tags/tag_create_container';
 
 class PhotoShow extends React.Component {
     constructor(props) {
@@ -35,6 +35,6 @@ class PhotoShow extends React.Component {
             </div>
         );
     }
-}
+}   
 
 export default PhotoShow;
