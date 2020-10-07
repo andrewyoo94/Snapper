8b3bdbb51c4bcf8588f237f02c9f7d1b14f346af
Andrew <andrewyoo.ny@gmail.com>
2020-10-07 18:53:58 -0400

    Added user_actions.js

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,16 @@
+import * as UserAPIUtil from '../util/user_api_util';
+
+export const RECEIVE_USER = "RECEIVE_USER";
+
+const receiveUser = user => ({
+    type: RECEIVE_USER,
+    user
+});
+
+export const fetchUser = (id) => dispatch => {
+    return (
+        UserAPIUtil.fetchUser(id)
+            .then((user) => dispatch(receiveUser(user)))
+            // .fail(error => dispatch(receiveUserErrors(error.responseJSON)))
+    )
+}
