ebb7f3f3eebd12b57330b251628ec32b63b96384
Andrew <andrewyoo.ny@gmail.com>
2020-10-21 23:43:12 -0400

    Added styling to homepage text

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -4,8 +4,11 @@ class HomePage extends React.Component {
     render() {
         return (
             <div className="homepage">
-                <h1>Find your inspiration.</h1>
-                <h2>Join the Flickr community, home to tens of billions of photos and 2 million groups.</h2>
+                <div className="homepage-text">
+                    <h1 className="hometext-h1">Find your inspiration.</h1>
+                    <h3 className="hometext-h3">Join the Flickr community, home to tens of billions of 
+                    photos and 2 million groups.</h3>
+                </div>
             </div>
         );
     };
