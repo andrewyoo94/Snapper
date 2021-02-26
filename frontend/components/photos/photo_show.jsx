08e44f11c6eedcedef124f4eeec0f4c43494c8ba
Andrew <andrewyoo.ny@gmail.com>
2021-02-25 19:36:48 -0500

    Added photo index image

--- frontend/components/photos/photo_show.jsx
+++ frontend/components/photos/photo_show.jsx
@@ -53,22 +53,12 @@ class PhotoShow extends React.Component {
         }
         this.setState({ editable: !this.state.editable })
 
-        // let editedPhoto = this.props.photo;
-        // let editedPhoto = { id: this.props.photoId, title: newTitle, description: newDescription, photographer_id: this.props.photo.photographer_id, photoUrl: this.props.photo.photoUrl }
-
         let editedPhoto = { 
             photo: { id: this.props.photoId, title: newTitle, description: newDescription},
             id: this.props.photoId
         };
-        // let editedPhoto = { id: this.props.photoId, title: newTitle, description: newDescription };
-
-        // const photo = Object.assign({}, editedPhoto);
-
-        // let editedPhoto = { title: newTitle, description: newDescription }
-
 
         this.props.updatePhoto(editedPhoto)
-            // .then(() => this.props.history.push(`/photos/${editedPhoto.id}`))
             .then(() => this.props.fetchPhoto(this.props.photoId))
     }
 
